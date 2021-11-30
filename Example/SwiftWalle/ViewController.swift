5ebc9bcb3986e0ee19e212984b5c0176eae84075
mistdon <wonderland.don@gmail.com>
2021-11-30 17:39:28 +0800

    feat:add random color, upadte miniVersiton to iOS10

--- Example/SwiftWalle/ViewController.swift
+++ Example/SwiftWalle/ViewController.swift
@@ -19,17 +19,6 @@ class ViewController: UIViewController {
         }
         view.backgroundColor = UIColor(hex6: 0xeb3d34)
         
-        "aa".isBlank
-        
-//        let att = NSAttributedString(string: "Hello world!")
-//        att.high
-    
-        
-        var aa = ["a", "b"]
-        let indexp = IndexPath(row: 0, section: 0)
-        let res = aa(indexp)
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,7 +26,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        let att = NSAttributedString(string: "ss", attributes: [NSAttributedString.Key.foregroundColor: UIColor(hex6: "11111111111")])
         view.backgroundColor = UIColor(hex6: "1")
     }
 }
--- SwiftWalle/Classes/UIKit/UIColor+Extension.swift
+++ SwiftWalle/Classes/UIKit/UIColor+Extension.swift
@@ -19,6 +19,9 @@ public enum UIColorInputError : Error {
 }
 
 extension UIColor {
+    static var randomColor: UIColor {
+        return UIColor(red:.random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
+    }
     /// The shorthand three-digit hexadecimal representation of color.
     /// #RGB defines to the color #RRGGBB.
     /// - Parameters:
